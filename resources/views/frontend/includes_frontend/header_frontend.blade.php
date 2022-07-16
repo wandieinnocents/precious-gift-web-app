0f257998808fefd40299fee6e21a73a297e6fa20
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-07-16 15:44:14 +0300

    Modify header

--- resources/views/frontend/includes_frontend/header_frontend.blade.php
+++ resources/views/frontend/includes_frontend/header_frontend.blade.php
@@ -1,19 +1,18 @@
+<!-- Main Header -->
+<header class="main-header header-style-two">
+    <!-- Header Top -->
+    <div class="header-top">
+        <div class="auto-container">
+            <div class="inner clearfix">
+                <div class="top-left clearfix">
+                    <ul class="info clearfix">
+                        <li>We are here to support you</li>
+                    </ul>
+                </div>
 
-        <!-- Main Header -->
-        <header class="main-header header-style-two">
-            <!-- Header Top -->
-            <div class="header-top">
-                <div class="auto-container">
-                    <div class="inner clearfix">
-                        <div class="top-left clearfix">
-                            <ul class="info clearfix">
-                                <li>Become A Helping Hand For The Poor</li>
-                            </ul>
-                        </div>
-
-                        <div class="top-right clearfix">
-                            <ul class="info clearfix">
-                                <li>
+                <div class="top-right clearfix">
+                    <ul class="info clearfix">
+                        {{-- <li>
                                     <div class="change-language">
                                         <div class="language dropdown"><a class="dropdown-toggle clearfix"
                                                 id="lang-dropdown" data-toggle="dropdown" aria-haspopup="true"
@@ -28,138 +27,113 @@
                                             </ul>
                                         </div>
                                     </div>
-                                </li>
-                            </ul>
-                            <ul class="social-links clearfix">
-                                <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
-                                <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                <li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
-                                <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                                <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
+                                </li> --}}
+                    </ul>
+                    <ul class="social-links clearfix">
+                        <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+                        <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                        <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                    </ul>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <!-- Header Upper -->
-            <div class="header-upper">
-                <div class="auto-container">
-                    <div class="inner-container clearfix">
-                        <!--Logo-->
-                        <div class="logo-box">
-                            <div class="logo"><a href="index.html" title="Precious Gift"><img
-                                        src="assets/frontend_assets/images/logo-2.png"
-                                        alt="Precious Gift"
-                                        title="Precious Gift"></a></div>
-                        </div>
+    <!-- Header Upper -->
+    <div class="header-upper">
+        <div class="auto-container">
+            <div class="inner-container clearfix">
+                <!--Logo-->
+                <div class="logo-box">
+                    <div class="logo"><a href="index.html" title="Precious Gift"><img
+                                src="assets/frontend_assets/images/logo-2.png" alt="Precious Gift"
+                                title="Precious Gift"></a></div>
+                </div>
 
-                        <!--Nav Box-->
-                        <div class="nav-outer clearfix">
-                            <!--Mobile Navigation Toggler-->
-                            <div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
+                <!--Nav Box-->
+                <div class="nav-outer clearfix">
+                    <!--Mobile Navigation Toggler-->
+                    <div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
 
-                            <!-- Main Menu -->
-                            <nav class="main-menu navbar-expand-md navbar-light">
-                                <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
-                                    <ul class="navigation clearfix">
-                                        <li class="current dropdown"><a href="index.html">Home</a>
-                                            <ul>
-                                                <li><a href="index.html">Home Charity</a></li>
-                                                <li><a href="index-2.html">Home Volunteer</a></li>
-                                                <li><a href="index-3.html">Home Wildlife</a></li>
-                                                <li><a href="index-4.html">Home Environment</a></li>
-                                                <li class="dropdown"><a href="index.html">Header Styles</a>
-                                                    <ul>
-                                                        <li><a href="index.html">Header Style One</a></li>
-                                                        <li><a href="index-2.html">Header Style Two</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="dropdown"><a href="about.html">About</a>
-                                            <ul>
-                                                <li><a href="about.html">About Us</a></li>
-                                                <li><a href="volunteers.html">Our Volunteers</a></li>
-                                                <li><a href="faq.html">Faq's</a></li>
-                                                <li><a href="donate.html">Make Donation</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="dropdown"><a href="causes.html">Causes</a>
-                                            <ul>
-                                                <li><a href="causes.html">See Causes</a></li>
-                                                <li><a href="cause-single.html">Cause Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="dropdown"><a href="events.html">Pages</a>
-                                            <ul>
-                                                <li><a href="events.html">All Events</a></li>
-                                                <li><a href="event-single.html">Event Details</a></li>
-                                                <li><a href="donate.html">Make Donation</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="dropdown"><a href="blog.html">Blog</a>
-                                            <ul>
-                                                <li><a href="blog.html">Visit Our Blog</a></li>
-                                                <li><a href="blog-single.html">Blog Post Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="contact.html">Contact</a></li>
+                    <!-- Main Menu -->
+                    <nav class="main-menu navbar-expand-md navbar-light">
+                        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                            <ul class="navigation clearfix">
+                                <li class="current dropdown"><a href="index.html">Home</a></li>
+                                <li class="dropdown"><a href="javascript::void(0)">Know Us</a>
+                                    <ul>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="volunteers.html">Our Team</a></li>
+                                        <li><a href="faq.html">Faq's</a></li>
                                     </ul>
-                                </div>
-                            </nav>
-                            <!-- Main Menu End-->
+                                </li>
+                                
+                                <li class="dropdown"><a href="blog.html">Blog</a>
+                                    <ul>
+                                        <li><a href="blog.html">Visit Our Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Post Details</a></li>
+                                    </ul>
+                                </li>
 
-                            <div class="donate-link">
-                                <a href="donate.html" class="theme-btn btn-style-one"><span class="btn-title">Donate
-                                        Now</span></a>
-                            </div>
+                                
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
                         </div>
+                    </nav>
+                    <!-- Main Menu End-->
+
+                    <div class="donate-link">
+                        <a href="donate.html" class="theme-btn btn-style-one"><span class="btn-title">Donate
+                                Now</span></a>
                     </div>
                 </div>
             </div>
-            <!--End Header Upper-->
+        </div>
+    </div>
+    <!--End Header Upper-->
 
-            <!-- Sticky Header  -->
-            <div class="sticky-header">
-                <div class="auto-container clearfix">
-                    <!--Logo-->
-                    <div class="logo pull-left">
-                        <a href="index.html" title=""><img src="assets/frontend_assets/images/sticky-logo.png"
-                                alt="" title=""></a>
-                    </div>
-                    <!--Right Col-->
-                    <div class="pull-right">
-                        <!-- Main Menu -->
-                        <nav class="main-menu clearfix">
-                            <!--Keep This Empty / Menu will come through Javascript-->
-                        </nav><!-- Main Menu End-->
-                    </div>
-                </div>
-            </div><!-- End Sticky Menu -->
+    <!-- Sticky Header  -->
+    <div class="sticky-header">
+        <div class="auto-container clearfix">
+            <!--Logo-->
+            <div class="logo pull-left">
+                <a href="index.html" title=""><img src="assets/frontend_assets/images/sticky-logo.png"
+                        alt="" title=""></a>
+            </div>
+            <!--Right Col-->
+            <div class="pull-right">
+                <!-- Main Menu -->
+                <nav class="main-menu clearfix">
+                    <!--Keep This Empty / Menu will come through Javascript-->
+                </nav><!-- Main Menu End-->
+            </div>
+        </div>
+    </div><!-- End Sticky Menu -->
 
-            <!-- Mobile Menu  -->
-            <div class="mobile-menu">
-                <div class="menu-backdrop"></div>
-                <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
+    <!-- Mobile Menu  -->
+    <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
+        <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
 
-                <nav class="menu-box">
-                    <div class="nav-logo"><a href="index.html"><img src="assets/frontend_assets/images/nav-logo.png"
-                                alt="" title=""></a></div>
-                    <div class="menu-outer">
-                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
-                    </div>
-                    <!--Social Links-->
-                    <div class="social-links">
-                        <ul class="clearfix">
-                            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                            <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                            <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                            <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                            <li><a href="#"><span class="fab fa-youtube"></span></a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div><!-- End Mobile Menu -->
-        </header>
-        <!-- End Main Header -->
+        <nav class="menu-box">
+            <div class="nav-logo"><a href="index.html"><img src="assets/frontend_assets/images/nav-logo.png"
+                        alt="" title=""></a></div>
+            <div class="menu-outer">
+                <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+            </div>
+            <!--Social Links-->
+            <div class="social-links">
+                <ul class="clearfix">
+                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                    <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div><!-- End Mobile Menu -->
+</header>
+<!-- End Main Header -->
