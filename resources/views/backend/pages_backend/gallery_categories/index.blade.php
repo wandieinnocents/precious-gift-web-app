f7a4af740ea85ae9aa825361009786a72bf5bfa7
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-07-19 22:44:23 +0300

    View gallery category details

--- resources/views/backend/pages_backend/gallery_categories/index.blade.php
+++ resources/views/backend/pages_backend/gallery_categories/index.blade.php
@@ -1,116 +1,151 @@
 @extends('backend.layouts_backend.master')
 
 @section('title')
-
 @endsection
 
 @section('content')
-
-<!-- Main content dashboard  -->
-
-
-                       
-                <div class="page-content">
-                    <div class="container-fluid">
-
-                       
-
-                        <div class="row align-items-center">
-                            <div class="col-md-6">
-                                <div class="mb-3">
-                                    <h5 class="card-title">Gallery <span class="text-muted fw-normal ms-2">(1)</span></h5>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="d-flex flex-wrap align-items-center justify-content-end gap-2 mb-3">
-                                   
-                                    <div>
-                                        <a href="{{ route('gallery_categories.create') }}" class="btn bg-primary text-light"><i class="bx bx-plus me-1"></i> Add Photo</a>
-                                    </div>
-                                    
-                                   
-                                </div>
-
-                            </div>
-                        </div>
-                        <!-- end row -->
-
-                        <!-- FETCH FEEDBACKS -->
-
-                        <div class="table-responsive mb-4">
-                            <table class="table align-middle datatable dt-responsive table-check nowrap" style="border-collapse: collapse; border-spacing: 0 8px; width: 100%;">
-                                <thead>
-                                  <tr>
-                                    <th scope="col" style="width: 50px;">
-                                        <div class="form-check font-size-16">
-                                            <input type="checkbox" class="form-check-input" id="checkAll">
-                                            <label class="form-check-label" for="checkAll"></label>
-                                        </div>
-                                    </th>
-                                    <th scope="col"> #ID</th>
-                                    <th scope="col"> Name</th>
-                                    <th scope="col">Description</th>
-                                    <th style="width: 150px; min-width: 80px;">Action</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                   
-                                @foreach($gallery_categories as $gallery_category)
-                                      <tr>
-                                        <th scope="row">
-                                            <div class="form-check font-size-16">
-                                                <input type="checkbox" class="form-check-input" id="contacusercheck11">
-                                                <label class="form-check-label" for="contacusercheck11"></label>
-                                            </div>
-                                        </th>
-                                        <td>1</td>
-                                        <td>
-                                            <a href="#" class="text-body">{{  $gallery_category->gallery_category_name  }}</a>
-                                        </td>
-                                       
-                                        <td>{{  $gallery_category->gallery_category_description  }}</td>
-                                       
-                                        <td colspan="6">
-                                            <div class="row">
-                                             <div class="col-md-4">
-                                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap"><i class=" far fa-eye  "></i></button>
-                                            </div>
-                                                
-                                            <div class="col-md-4">
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editFoodOrder" data-bs-whatever="@getbootstrap"><i class="fas fa-pencil-alt "></i></button>
-
-                                            </div>
-                                            
-                                            <!-- delete food menu -->
-                                            <div class="col-md-4">
-                                            <form action="" method="post">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <a >  <button class="btn btn-danger shadow btn-xs sharp"> <span class="fa fa-trash"> </button> </a>  
-                                                        
-                                            </form>
+    <!-- Main content dashboard  -->
 
 
 
-                                        </div>
-                                            </div>
-                                        </td>
-                                      </tr>
-                                    @endforeach 
+    <div class="page-content">
+        <div class="container-fluid">
 
 
-                                </tbody>
-                            </table>
-                            <!-- end table -->
-                        </div>
-                        <!-- end table responsive -->
-                        
-                    </div> <!-- container-fluid -->
-                </div>
-                <!-- End Page-content -->
+
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <h5 class="card-title">Gallery <span class="text-muted fw-normal ms-2">(1)</span></h5>
                     </div>
-                    <!-- container-fluid -->
                 </div>
 
+                <div class="col-md-6">
+                    <div class="d-flex flex-wrap align-items-center justify-content-end gap-2 mb-3">
+
+                        <div>
+                            <a href="{{ route('gallery_categories.create') }}" class="btn bg-primary text-light"><i
+                                    class="bx bx-plus me-1"></i> Add Photo</a>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+            <!-- end row -->
+
+            <!-- FETCH DATA -->
+
+            <div class="table-responsive mb-4">
+                <table class="table align-middle datatable dt-responsive table-check nowrap"
+                    style="border-collapse: collapse; border-spacing: 0 8px; width: 100%;">
+                    <thead>
+                        <tr>
+                            <th scope="col" style="width: 50px;">
+                                <div class="form-check font-size-16">
+                                    <input type="checkbox" class="form-check-input" id="checkAll">
+                                    <label class="form-check-label" for="checkAll"></label>
+                                </div>
+                            </th>
+                            <th scope="col"> #ID</th>
+                            <th scope="col"> Name</th>
+                            <th scope="col">Description</th>
+                            <th style="width: 150px; min-width: 80px;">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($gallery_categories as $gallery_category)
+                            <tr>
+                                <th scope="row">
+                                    <div class="form-check font-size-16">
+                                        <input type="checkbox" class="form-check-input" id="contacusercheck11">
+                                        <label class="form-check-label" for="contacusercheck11"></label>
+                                    </div>
+                                </th>
+                                <td>{{ $gallery_category->id }}</td>
+                                <td>
+                                    <a href="#" class="text-body">{{ $gallery_category->gallery_category_name }}</a>
+                                </td>
+                                <td>{{ $gallery_category->gallery_category_description }}</td>
+                                <td colspan="6">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <button type="button" class="btn btn-success" data-bs-toggle="modal"
+                                                data-bs-target="#exampleModal{{ $gallery_category->id }}" data-bs-whatever="@getbootstrap"><i
+                                                    class=" far fa-eye  "></i></button>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                                data-bs-target="#editFoodOrder" data-bs-whatever="@getbootstrap"><i
+                                                    class="fas fa-pencil-alt "></i></button>
+
+                                        </div>
+
+                                        <!-- delete food menu -->
+                                        <div class="col-md-4">
+                                            <form action="" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <a> <button class="btn btn-danger shadow btn-xs sharp"> <span
+                                                            class="fa fa-trash"> </button> </a>
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                             <!-- VIEW DETAILS MODEL -->
+                            <div class="modal fade" id="exampleModal{{ $gallery_category->id }}" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Gallery Category Details</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+
+
+
+                                        <div class="modal-body">
+
+                                            
+                                            <p>Category Name : {{ $gallery_category->gallery_category_name }}</p>
+                                            <hr>
+
+                                            
+                                            <p>Description : {{ $gallery_category->gallery_category_description }}</p>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close
+                                            </button>
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                                data-bs-target="#editFoodOrder{{ $gallery_category->id }}"
+                                                data-bs-whatever="@getbootstrap">Edit Category</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END OF VIEW DETAILS MODEL -->
+
+                        @endforeach
+
+
+                    </tbody>
+                </table>
+                <!-- end table -->
+            </div>
+            <!-- end table responsive -->
+
+        </div> <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+    </div>
+    <!-- container-fluid -->
+    </div>
 @endsection
