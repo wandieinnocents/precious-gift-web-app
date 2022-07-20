85f857893f1f6d73f4447ce3de05cca74b81aae5
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-07-20 21:13:06 +0300

    Modify post category model

--- app/Models/PostCategory.php
+++ app/Models/PostCategory.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class PostCategory extends Model
 {
     use HasFactory;
+    protected $fillables = [
+        'post_category_name',
+        'post_category_description'
+
+    ];
+
+     // relationship
+     public function post_r(){
+        return $this->hasMany(Post::class);
+    }
 }
