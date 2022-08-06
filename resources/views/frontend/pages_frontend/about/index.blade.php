7a068903b4893348e58ae01c290f7477dd7a0324
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-08-06 17:23:34 +0300

    Refactor code

--- resources/views/frontend/pages_frontend/about/index.blade.php
+++ resources/views/frontend/pages_frontend/about/index.blade.php
@@ -81,45 +81,51 @@
                             src="assets/frontend_assets/images/resource/featured-image-5.jpg" alt="" /></figure>
                 </div>
             </div>
+            {{-- about 2 --}}
+            <div class="row clearfix">
+                <!--Text Column-->
+                <div class="text-column col-lg-6 col-md-12 col-sm-12">
+                    <div class="inner">
+                        <div class="lower-text">
+                        Precious Child Development Centre is an organization based in a rural area
+                            and its major aim is helping the vulnerable children and the community at large (in poverty
+                            stricken households) so as to enhance development and to improve livelihoods. PCDC operates in
+                            Kibale Parish, Bulera Sub-County in Mityana District and Kakiri in Wakiso District in Uganda.
 
-            <!--Featured Blocks-->
-            <div class="featured-blocks">
-                <div class="row clearfix">
-                    <!--Featured Block-->
-                    <div class="featured-block col-lg-4 col-md-6 col-sm-12">
-                        <div class="inner-box">
-                            <div class="inner">
-                                <div class="icon"><span class="flaticon-care"></span></div>
-                                <h3>Helping Mankind</h3>
-                                <div class="text">Auis nostrud exercitation ullam labor offica aliquip exsed consequat
-                                    duis autpsum natus dolore magna aliqua minim ipsum.</div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--Featured Block-->
-                    <div class="featured-block col-lg-4 col-md-6 col-sm-12">
-                        <div class="inner-box">
-                            <div class="inner">
-                                <div class="icon"><span class="flaticon-pigeon"></span></div>
-                                <h3>Love Your World</h3>
-                                <div class="text">Auis nostrud exercitation ullam labor offica aliquip exsed consequat
-                                    duis autpsum natus dolore magna aliqua minim ipsum.</div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--Featured Block-->
-                    <div class="featured-block col-lg-4 col-md-6 col-sm-12">
-                        <div class="inner-box">
-                            <div class="inner">
-                                <div class="icon"><span class="flaticon-harvest"></span></div>
-                                <h3>Food for Everyone</h3>
-                                <div class="text">Auis nostrud exercitation ullam labor offica aliquip exsed consequat
-                                    duis autpsum natus dolore magna aliqua minim ipsum.</div>
-                            </div>
+                            Kibaale Parish is located in Bulera sub-county, Mityana district of the republic of Uganda, 20km
+                            from Mityana Taxi Park. The parish serves as a hub for ten villages of Buleeya, Bulwanyamayinja,
+                            Bombo, Buligiya, Buwalantaama, Nalulangadde, Lubanyi, Kigabi, Kayunga, and Lusaana. The Parish
+                            is identified with scarce poor water sources, poor accommodation, poor sanitation, and high
+                            numbers of school dropouts and young mothers. A primary school which gives foundation to the
+                            rest of the villages and a small healthy centre
                         </div>
                     </div>
                 </div>
+                <!--Image Column-->
+                <div class=" col-lg-6 col-md-12 col-sm-12">
+                    <div class="inner">
+
+                        <div class="lower-text">
+                            with limited out patient services which
+                            according to the community was donated by area Member of Parliament also serving the rest of the
+                            communities. The core values of the organization are integrity, stewardship, excellence, and
+                            dignity. Our
+                            objectives are To educate, support and monitor the registered children and their families
+                            towards a sustainable healthy and development.
+
+                            To serve as children advocate in all spheres of life. We shall pursue our objectives basing on,
+                            Physical and health development Social and health development Cognitive livelihood and education
+                            development
+
+                            PCDC provides clothes to the children, pays school fees for the children under the organization,
+                            provide scholastic materials that are needed by the children. we at PCDC also help the disabled
+                            children and women, the ederly within the community so as to give them hope and joy so that they
+                            also know that there is someone who thinks about them and loves them too.</div>
+                    </div>
+                </div>
             </div>
+        </div>
+        
         </div>
     </section>
 
@@ -132,16 +138,16 @@
                     <!--Column-->
                     <div class="counter-column col-lg-3 col-md-6 col-sm-12 wow fadeInUp">
                         <div class="inner-box">
-                            <div class="count-box"><span class="count-text" data-speed="3000" data-stop="98520">0</span>
+                            <div class="count-box"><span class="count-text" data-speed="3000" data-stop="30">0</span>
                             </div>
-                            <div class="counter-title">raised Donations</div>
+                            <div class="counter-title">CITIES</div>
                         </div>
                     </div>
 
                     <!--Column-->
                     <div class="counter-column col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="400ms">
                         <div class="inner-box">
-                            <div class="count-box"><span class="count-text" data-speed="1500" data-stop="306">0</span></div>
+                            <div class="count-box"><span class="count-text" data-speed="306" data-stop="306">0</span></div>
                             <div class="counter-title">Causes Solved</div>
                         </div>
                     </div>
@@ -149,7 +155,8 @@
                     <!--Column-->
                     <div class="counter-column col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="800ms">
                         <div class="inner-box">
-                            <div class="count-box"><span class="count-text" data-speed="1500" data-stop="84">0</span></div>
+                            <div class="count-box"><span class="count-text" data-speed="3000" data-stop="14">0</span>
+                            </div>
                             <div class="counter-title">Volunteers</div>
                         </div>
                     </div>
@@ -157,7 +164,7 @@
                     <!--Column-->
                     <div class="counter-column col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="1200ms">
                         <div class="inner-box">
-                            <div class="count-box"><span class="count-text" data-speed="3000" data-stop="1369">0</span>
+                            <div class="count-box"><span class="count-text" data-speed="3000" data-stop="12">0</span>
                             </div>
                             <div class="counter-title">Projects Done</div>
                         </div>
@@ -176,7 +183,7 @@
         <div class="auto-container">
             <div class="row clearfix">
                 <div class="title-column col-xl-9 col-lg-12 col-md-12 col-sm-12">
-                    <h2>Become a vital part of Kausid &amp; make a real difference to lives of many!</h2>
+                    <h2>Become a our Community to Change the world</h2>
                 </div>
                 <div class="link-column col-xl-3 col-lg-12 col-md-12 col-sm-12">
                     <div class="link-box">
