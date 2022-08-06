94abb084f747a8d799878eab5b92371eb7928c29
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-08-06 18:51:45 +0300

    Modify footer

--- resources/views/frontend/includes_frontend/footer.blade.php
+++ resources/views/frontend/includes_frontend/footer.blade.php
@@ -1,145 +1,128 @@
  <footer class="main-footer">
-            <div class="image-layer" style="background-image:url('assets/frontend_assets/images/background/footer-bg.jpg')">
-            </div>
+     <div class="image-layer" style="background-image:url('assets/frontend_assets/images/background/footer-bg.jpg')">
+     </div>
 
-            <div class="auto-container">
-                <!--Widgets Section-->
-                <div class="widgets-section">
-                    <div class="row clearfix">
+     <div class="auto-container">
+         <!--Widgets Section-->
+         <div class="widgets-section">
+             <div class="row clearfix">
 
-                        <!--Column-->
-                        <div class="column big-column col-lg-4 col-md-12 col-sm-12">
-                            <div class="footer-widget logo-widget">
-                                <div class="widget-content">
-                                    <div class="footer-logo">
-                                        <a href="index.html"><img src="assets/frontend_assets/images/footer-logo.png"
-                                                alt="" /></a>
-                                    </div>
-                                    <div class="text">Alit usmod tempor incididunt laboret quis arud exercitation
-                                        ullamcos laboris nisiut aliquip. exa consequat duis aute rue dolor prehendrit
-                                        lorem esse cillum dolore eu fugiat nulla.</div>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
-                                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                        <li><a href="#"><span class="fab fa-vimeo-v"></span></a></li>
-                                        <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                                        <li><a href="#"><span class="fab fa-youtube"></span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Column-->
-                        <div class="big-column col-lg-8 col-md-12 col-sm-12">
-                            <div class="row clearfix">
+                 <!--Column-->
+                 <div class="column big-column col-lg-4 col-md-12 col-sm-12">
+                     <div class="footer-widget logo-widget">
+                         <div class="widget-content">
+                         <h3>About Us</h3>
+                             <div class="text">Precious Child Development Centre is an organization based in a rural
+                                 area and its major aim is helping the vulnerable children and the community at large
+                                 (in poverty stricken households) so as to enhance development and to improve
+                                 livelihoods. PCDC operates in Kibale Parish, Bulera Sub-County in Mityana District and
+                                 Kakiri in Wakiso District in Uganda.</div>
+                            
+                         </div>
+                     </div>
+                 </div>
+                 <!--Column-->
+                 <div class="big-column col-lg-8 col-md-12 col-sm-12">
+                     <div class="row clearfix">
 
-                                <!--Column-->
-                                <div class="column col-lg-4 col-md-4 col-sm-12">
-                                    <div class="footer-widget links-widget">
-                                        <div class="widget-content">
-                                            <h3>About Us</h3>
-                                            <ul>
-                                                <li><a href="#">Upcoming Events</a></li>
-                                                <li><a href="#">Latest Causes</a></li>
-                                                <li><a href="#">News Blog</a></li>
-                                                <li><a href="#">Contact Us</a></li>
-                                                <li><a href="#">Who We Are</a></li>
-                                                <li><a href="#">Connect With Us</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+                         <!--Column-->
+                         <div class="column col-lg-4 col-md-4 col-sm-12">
+                             <div class="footer-widget links-widget">
+                                 <div class="widget-content">
+                                     <h3>Quick Links</h3>
+                                     <ul>
+                                         <li><a href="#">About</a></li>
+                                         <li><a href="#">Team</a></li>
+                                         <li><a href="#">News</a></li>
+                                         <li><a href="#">Faq's</a></li>
+                                         <li><a href="#">Who We Are</a></li>
+                                         <li><a href="#">Connect With Us</a></li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
 
-                                <!--Column-->
-                                <div class="column col-lg-4 col-md-4 col-sm-12">
-                                    <div class="footer-widget links-widget">
-                                        <div class="widget-content">
-                                            <h3>Our Partners</h3>
-                                            <ul>
-                                                <li><a href="#">Donate Kausid</a></li>
-                                                <li><a href="#">Save Our earth</a></li>
-                                                <li><a href="#">Water Shortages</a></li>
-                                                <li><a href="#">Recycle Better</a></li>
-                                                <li><a href="#">Plant Trees Welfare</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+                         <!--Column-->
+                         <div class="column col-lg-4 col-md-4 col-sm-12">
+                             <div class="footer-widget links-widget">
+                                 <div class="widget-content">
+                                     <h3>Our Partners</h3>
+                                     <ul>
+                                         <li><a href="#">Donate Kausid</a></li>
+                                         <li><a href="#">Save Our earth</a></li>
+                                         <li><a href="#">Water Shortages</a></li>
+                                         <li><a href="#">Recycle Better</a></li>
+                                         <li><a href="#">Plant Trees Welfare</a></li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
 
-                                <!--Column-->
-                                <div class="column col-lg-4 col-md-4 col-sm-12">
-                                    <div class="footer-widget links-widget">
-                                        <div class="widget-content">
-                                            <h3>Explore</h3>
-                                            <ul>
-                                                <li><a href="#">Adopt A Child</a></li>
-                                                <li><a href="#">Join Our Programs</a></li>
-                                                <li><a href="#">Food Crisis Looms</a></li>
-                                                <li><a href="#">Make Donation</a></li>
-                                                <li><a href="#">Press Releases</a></li>
-                                                <li><a href="#">Global Programs</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+                         <!--Column-->
+                         <div class="column col-lg-4 col-md-4 col-sm-12">
+                             <div class="footer-widget links-widget">
+                                 <div class="widget-content">
+                                     <h3>Explore</h3>
+                                     <ul>
+                                         <li><a href="#">Adopt A Child</a></li>
+                                         <li><a href="#">Join Our Programs</a></li>
+                                         <li><a href="#">Food Crisis Looms</a></li>
+                                         <li><a href="#">Make Donation</a></li>
+                                         <li><a href="#">Press Releases</a></li>
+                                         <li><a href="#">Global Programs</a></li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
 
-                            </div>
-                        </div>
+                     </div>
+                 </div>
 
-                        <!--Column-->
-                        <div class="big-column col-lg-12 col-md-12 col-sm-12">
-                            <div class="row clearfix">
-                                <!--Column-->
-                                <div class="column col-lg-4 col-md-12 col-sm-12">
-                                    <div class="footer-widget info-widget">
-                                        <div class="widget-content">
-                                            <h3>Contact Us</h3>
-                                            <ul class="contact-info">
-                                                <li>4291 Glenview Drive, Florence, MI 29501</li>
-                                                <li><a href="tel:812-070-3692"><span class="fa fa-phone-alt"></span>
-                                                        Phone 812-070-3692</a></li>
-                                                <li><a href="mailto:donations@example.org"><span
-                                                            class="fa fa-envelope-open"></span>
-                                                        donations@example.org</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--Column-->
-                                <div class="column col-lg-8 col-md-12 col-sm-12">
-                                    <div class="footer-widget info-widget">
-                                        <div class="widget-content">
-                                            <h3>Subscribe Our Newsletter For Updates</h3>
-                                            <div class="newsletter-form">
-                                                <form method="post"
-                                                    action="http://t.commonsupport.xyz/kausid/contact.html">
-                                                    <div class="form-group clearfix">
-                                                        <input type="email" name="email" value=""
-                                                            placeholder="Enter Your Email" required="">
-                                                        <button type="submit"
-                                                            class="theme-btn newsletter-btn">Subscribe</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                 <!--Column-->
+                 <div class="big-column col-lg-12 col-md-12 col-sm-12">
+                     <div class="row clearfix">
+                         <!--Column-->
+                         <div class="column col-lg-3 col-md-12 col-sm-12">
+                             
+                         </div>
+                         <!--Column-->
+                         <div class="column col-lg-6 col-md-12 col-sm-12">
+                             <div class="footer-widget info-widget">
+                                 <div class="widget-content">
+                                     <h3>Subscribe Our Newsletter For Updates</h3>
+                                     <div class="newsletter-form">
+                                         <form method="post" action="http://t.commonsupport.xyz/kausid/contact.html">
+                                             <div class="form-group clearfix">
+                                                 <input type="email" name="email" value=""
+                                                     placeholder="Enter Your Email" required="">
+                                                 <button type="submit"
+                                                     class="theme-btn newsletter-btn">Subscribe</button>
+                                             </div>
+                                         </form>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                          <div class="column col-lg-3 col-md-12 col-sm-12">
+                             
+                         </div>
+                     </div>
+                 </div>
 
-                    </div>
+             </div>
 
-                </div>
-            </div>
+         </div>
+     </div>
 
-            <!-- Footer Bottom -->
-            <div class="footer-bottom">
-                <div class="auto-container">
-                    <div class="inner">
-                        <!--Copyright-->
-                        <div class="copyright">Copyrights (c) 2022 <a href="#">Precious Gift : </a>
-                            Design by : Wanran Inc.</div>
-                    </div>
-                </div>
-            </div>
+     <!-- Footer Bottom -->
+     <div class="footer-bottom">
+         <div class="auto-container">
+             <div class="inner">
+                 <!--Copyright-->
+                 <div class="copyright">Copyrights (c) 2022 <a href="#">Precious Gift : </a>
+                     Design by : Wanran Inc.</div>
+             </div>
+         </div>
+     </div>
 
-        </footer>
+ </footer>
