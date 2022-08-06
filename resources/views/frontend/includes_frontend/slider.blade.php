40275e09e7da54799ad456c5bbde52280c74e01d
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-08-06 19:22:07 +0300

    Modify slider section

--- resources/views/frontend/includes_frontend/slider.blade.php
+++ resources/views/frontend/includes_frontend/slider.blade.php
@@ -1,73 +1,75 @@
 <section class="banner-section style-two">
-            <div class="banner-carousel kausid-carousel owl-theme owl-carousel"
-                data-options='{"loop": true, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 300, "responsive":{ "0" :{ "items": "1" }, "768" :{ "items" : "1" } , "1000":{ "items" : "1" }}}'>
-                <!-- Slide Item -->
-                <div class="slide-item">
-                    <div class="image-layer"
-                        style="background-image:url('assets/frontend_assets/images/main-slider/4.jpg')"></div>
-                    <div class="auto-container">
-                        <div class="content-box">
-                            <div class="content">
-                                <div class="inner">
-                                    <div class="sub-title">we support the community</div>
-                                    <h2>We empower lives of people with need</h2>
-                                    
-                                    <div class="links-box">
-                                        <a href="/contact/create" class="theme-btn btn-style-two"><span
-                                                class="btn-title">CONTACT</span></a>
-                                        {{-- <span class="theme-icon"></span> --}}
-                                        <a href="/donate/create" class="theme-btn btn-style-three"><span
-                                                class="btn-title">Donate</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!-- Slide Item -->
-                <div class="slide-item">
-                    <div class="image-layer"
-                        style="background-image:url('assets/frontend_assets/images/main-slider/5.jpg')"></div>
-                    <div class="auto-container">
-                        <div class="content-box">
-                            <div class="content">
-                                <div class="inner">
-                                    <div class="sub-title">support the causes that matters</div>
-                                    <h2>Empowering the lives of needy people</h2>
-                                    <div class="links-box">
-                                        <a href="donate.html" class="theme-btn btn-style-two"><span
-                                                class="btn-title">Get involved</span></a>
-                                        <span class="theme-icon"></span>
-                                        <a href="about.html" class="theme-btn btn-style-three"><span
-                                                class="btn-title">who we are</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Slide Item -->
-                <div class="slide-item">
-                    <div class="image-layer"
-                        style="background-image:url('assets/frontend_assets/images/main-slider/6.jpg')"></div>
-                    <div class="auto-container">
-                        <div class="content-box">
-                            <div class="content">
-                                <div class="inner">
-                                    <div class="sub-title">support the causes that matters</div>
-                                    <h2>Empowering the lives of needy people</h2>
-                                    <div class="links-box">
-                                        <a href="donate.html" class="theme-btn btn-style-two"><span
-                                                class="btn-title">Get involved</span></a>
-                                        <span class="theme-icon"></span>
-                                        <a href="about.html" class="theme-btn btn-style-three"><span
-                                                class="btn-title">who we are</span></a>
-                                    </div>
-                                </div>
+    <div class="banner-carousel kausid-carousel owl-theme owl-carousel"
+        data-options='{"loop": true, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 6000, "smartSpeed": 300, "responsive":{ "0" :{ "items": "1" }, "768" :{ "items" : "1" } , "1000":{ "items" : "1" }}}'>
+        <!-- Slide Item -->
+        <div class="slide-item">
+            <div class="image-layer" style="background-image:url('assets/frontend_assets/images/main-slider/4.jpg')">
+            </div>
+            <div class="auto-container">
+                <div class="content-box">
+                    <div class="content">
+                        <div class="inner">
+                            <div class="sub-title">we support the community</div>
+                            <h2>We empower lives of people with need</h2>
+
+                            <div class="links-box">
+                                <a href="/contact/create" class="theme-btn btn-style-two"><span
+                                        class="btn-title">CONTACT</span></a>
+                                {{-- <span class="theme-icon"></span> --}}
+                                <a href="/donate/create" class="theme-btn btn-style-three"><span
+                                        class="btn-title">Donate</span></a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+
+        <!-- Slide Item -->
+        <div class="slide-item">
+            <div class="image-layer" style="background-image:url('assets/frontend_assets/images/main-slider/5.jpg')">
+            </div>
+            <div class="auto-container">
+                <div class="content-box">
+                    <div class="content">
+                        <div class="inner">
+                            <div class="sub-title">we can save the world together</div>
+                            <h2>We change peoples lives , one at a time.</h2>
+                             <div class="links-box">
+                                <a href="/contact/create" class="theme-btn btn-style-two"><span
+                                        class="btn-title">CONTACT</span></a>
+                                {{-- <span class="theme-icon"></span> --}}
+                                <a href="/donate/create" class="theme-btn btn-style-three"><span
+                                        class="btn-title">Donate</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Slide Item -->
+        <div class="slide-item">
+            <div class="image-layer" style="background-image:url('assets/frontend_assets/images/main-slider/6.jpg')">
+            </div>
+            <div class="auto-container">
+                <div class="content-box">
+                    <div class="content">
+                        <div class="inner">
+                            <div class="sub-title">we implement projects for the better</div>
+                            <h2>Running projects that benefit the community</h2>
+                            <div class="links-box">
+                                <a href="donate.html" class="theme-btn btn-style-two"><span class="btn-title">Get
+                                        involved</span></a>
+                                <span class="theme-icon"></span>
+                                <a href="about.html" class="theme-btn btn-style-three"><span class="btn-title">who we
+                                        are</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
