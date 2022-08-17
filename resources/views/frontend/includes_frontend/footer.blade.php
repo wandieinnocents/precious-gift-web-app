8b393ed3d4f6e12deb6ff2d7509b6358ad4bba92
“wandieinnocents” <wandieinnocent2@gmail.com>
2022-08-18 00:13:36 +0300

    Modify contacts

--- resources/views/frontend/includes_frontend/footer.blade.php
+++ resources/views/frontend/includes_frontend/footer.blade.php
@@ -1,9 +1,7 @@
  <footer class="main-footer">
      <div class="image-layer" style="background-image:url('assets/frontend_assets/images/background/foot.png')">
      </div>
-
      <div class="auto-container">
-
          <div>
              <div class="row" style="padding-top:30px;">
                  <div class="col-md-4">
@@ -12,25 +10,7 @@
                              <h3>About Us</h3>
                              <div class="text">Precious Child Development Centre is an organization based in a rural
                                  area and its major aim is helping the vulnerable children and the community at large
-                                 (in poverty stricken households) so as to enhance development and to improve
-                                 livelihoods. PCDC operates in Kibale Parish, Bulera Sub-County in Mityana District and
-                                 Kakiri in Wakiso District in Uganda.</div>
-
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-md-4">
-                     <div class="footer-widget links-widget">
-                         <div class="widget-content">
-                             <h3>Quick Links</h3>
-                             <ul>
-                                 <li><a href="#">About</a></li>
-                                 <li><a href="#">Team</a></li>
-                                 <li><a href="#">News</a></li>
-                                 <li><a href="#">Faq's</a></li>
-                                 <li><a href="#">Who We Are</a></li>
-                                 <li><a href="#">Connect With Us</a></li>
-                             </ul>
+                                 (in poverty stricken households) so as to enhance development.</div>
                          </div>
                      </div>
                  </div>
@@ -38,43 +18,56 @@
                      <div class="footer-widget links-widget">
                          <div class="widget-content">
                              <div class="inner">
-                                 <h3>Emails</h3>
+                             {{-- German --}}
+                                 <h3>German Contacts</h3>
                                  <ul class="info">
-                                     <li><span class="fa fa-map-marker-alt"></span> C/O Assy Nakiyingi Bahner
-                                         Friedrich-Heller str 15
-                                         30455 Hannover
-                                     </li>
-                                     <li><span class="fa fa-phone-alt"></span> <a href="tel:812-070-3692"> Phone
-                                             +49-1521-0172509 | +256-706-897541 </a></li>
-                                     <li><span class="fa fa-envelope-open"></span> <a
-                                             href="mailto:info@precious-gift.org">info@precious-gift.org</a></li>
-                                     <li><span class="fa fa-envelope-open"></span> <a
-                                             href="mailto:nakiyingiassybahner@gmail.com">Nakiyingiassybahner@gmail.com </a>
-                                     </li>
+                                <br> 
+                                <li><span class="fa fa-user"></span>    Assy Kanya Nakiyingi Bahner </li>
+                                <li><span class="fa fa-map-marker-alt"></span>
+                                    Friedrich-Heller str. 15
+                                </li>
+                                <li><span class="fa fa-phone-alt"></span> 
+                                <a href="tel:+49 (0) 15210172509">Phone :+49 (0) 15210172509 </a></li>
+                            </ul>
+                            {{-- Uganda --}}
+                           
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-md-4">
+                     <div class="footer-widget links-widget">
+                         <div class="widget-content">
+                             <div class="inner">
+                                 
+                                 {{-- Uganda --}}
+                                 <h3>Uganda Contacts</h3>
+                                 <ul class="info">
 
-                                     <li><span class="fa fa-envelope-open"></span> <a
-                                             href="mailto:Stevenssenkungu@precious-gift.org">Stevenssenkungu@precious-gift.org</a>
-                                     </li>
 
+                                     <li><span class="fa fa-map-marker-alt"></span>P.O Box 180027 Mityana , Uganda </li>
+                                     <li><span class="fa fa-map-marker-alt"></span>
+                                         Bulera, Mityana Uganda
+                                     </li>
+                                     <li><span class="fa fa-phone-alt"></span>
+                                         <a href="tel:+256 (0) 706-897541">Phone :+256(0) 706-897541 </a>
+                                     </li>
+                                     <li><a href="tel:+256 (0) 706-897541">Fax : +256 (0) 706-897541</a></li>
 
 
                                  </ul>
-
                              </div>
                          </div>
                      </div>
                  </div>
              </div>
-
          </div>
-
          <div class="row">
              <div class="col-md-3" style="visibility:hidden;"></div>
              <div class="col-md-6">
                  <div class="footer-widget info-widget">
                      <div class="widget-content">
                          <h3>Subscribe Our Newsletter</h3>
-
                          <div class="newsletter-form">
                              {{-- success or failre msg --}}
                              @if (\Session::has('success'))
@@ -103,11 +96,7 @@
              <div class="col-md-3" style="visibility:hidden;"></div>
          </div>
          <br>
-
-
-
      </div>
-
      <!-- Footer Bottom -->
      <div class="footer-bottom">
          <div class="auto-container">
@@ -118,5 +107,4 @@
              </div>
          </div>
      </div>
-
  </footer>
